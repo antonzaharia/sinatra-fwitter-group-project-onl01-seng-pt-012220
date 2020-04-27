a2ed87918d26bcca01c5fa4ac7433e4e7d7e9c34
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-27 16:48:24 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,7 @@
 class User < ActiveRecord::Base
+  include Slugafiable::InstanceMethods
+  extend Slugafiable::ClassMethods
   has_many :tweets
   has_secure_password
-
-  def slug
-    username = self.username
-    slug = username.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
-  end
-
-  def self.find_by_slug(slug)
-    @slug = slug
-    format_slug_beginning
-    results = self.where("username LIKE ?", @short_slug)
-    results.detect do |result|
-      result.slug === @slug
-    end
-  end
-
-  def self.format_slug_beginning
-    slug_beginning = @slug.split("-")[0]
-    slug_beginning.prepend("%")
-    slug_beginning << "%"
-    @short_slug = slug_beginning
-  end
+  validates :username, :email, :password, presence: true
 end
