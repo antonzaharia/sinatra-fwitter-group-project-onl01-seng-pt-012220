8bf28d97c0a6fe82c6dcb722196479115714f9db
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-27 16:42:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,19 +19,5 @@ class ApplicationController < Sinatra::Base
 
 
 
-  get '/users/:slug' do
-    slug = params[:slug]
-    @user = User.find_by_slug(slug)
-    erb :"users/show"
-  end
-
-  get '/logout' do
-    if Helpers.is_logged_in?(session)
-      session.clear
-      redirect to '/login'
-    else
-      redirect to '/'
-    end
-  end
 
 end
