b5993fdd6252c6c4dabdf031ab6f4d9f4867e7c3
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-27 16:13:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,20 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
-  get '/' do
-    erb :index
+
+
+  helpers do
+    def is_logged_in?(session)
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
